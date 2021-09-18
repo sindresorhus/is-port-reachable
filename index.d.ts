b318c3805e15d00356ad1d05d896e843390094b0
Michael Solomon <micheal540@gmail.com>
2021-09-18 17:34:31 +0300

    Update index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,2 +1,14 @@
+/**
+Check if a local or remote port is reachable
+@example
+```
+import isPortReachable from 'is-port-reachable';
+
+if(await isPortReachable(3000)) {
+	// start server
+}
+```
+*/
 declare function isPortReachable(port: number, opts?: {timeout: number, host: string}): Promise<boolean>;
+
 export default isPortReachable;
